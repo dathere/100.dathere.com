b5dac8413f8606fa92780db93303e15ff9e2f87f
rzmk <30333942+rzmk@users.noreply.github.com>
2025-09-16 07:17:50 -0400

    fix: use string values for analytics script

--- _static/analytics.js
+++ _static/analytics.js
@@ -1,11 +1,11 @@
 const analyticsScript = document.createElement("script");
 
 analyticsScript.setAttribute("src", "https://mk-analytics.dathere.com/api/script.js");
-analyticsScript.dataset["site-id"] = 4;
-analyticsScript.dataset["track-query"] = true;
-analyticsScript.dataset["session-replay"] = true;
-analyticsScript.dataset["track-errors"] = true;
-analyticsScript.async = true;
-analyticsScript.defer = true;
+analyticsScript.dataset["site-id"] = "4";
+analyticsScript.dataset["track-query"] = "true";
+analyticsScript.dataset["session-replay"] = "true";
+analyticsScript.dataset["track-errors"] = "true";
+analyticsScript.async = "true";
+analyticsScript.defer = "true";
 
-document.getElementsByTagName('head')[0].appendChild(analyticsScript);
+document.head.appendChild(analyticsScript);
