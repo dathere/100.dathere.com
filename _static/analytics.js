75c28ff852ef991bb0a7d4610205bc9648559018
rzmk <30333942+rzmk@users.noreply.github.com>
2025-09-16 07:22:58 -0400

    fix: use dot instead of brackets

--- _static/analytics.js
+++ _static/analytics.js
@@ -1,10 +1,10 @@
 const analyticsScript = document.createElement("script");
 
 analyticsScript.setAttribute("src", "https://mk-analytics.dathere.com/api/script.js");
-analyticsScript.dataset["site-id"] = "4";
-analyticsScript.dataset["track-query"] = "true";
-analyticsScript.dataset["session-replay"] = "true";
-analyticsScript.dataset["track-errors"] = "true";
+analyticsScript.dataset.siteId = "4";
+analyticsScript.dataset.webVitals = "true";
+analyticsScript.dataset.sessionReplay = "true";
+analyticsScript.dataset.trackErrors = "true";
 analyticsScript.async = "true";
 analyticsScript.defer = "true";
 
